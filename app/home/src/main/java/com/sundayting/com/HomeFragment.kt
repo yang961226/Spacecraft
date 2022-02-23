346feb972b4a59a9d13ebc83dcd523afdf65c5c8
SundayTing <524250484@qq.com>
2022-02-23 09:38:22 +0800

    提交首页、项目、我的页Module

---
+++ app/home/src/main/java/com/sundayting/com/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.sundayting.com
+
+import com.sundayting.com.home.R
+import com.sundayting.com.ui.BaseFragment
+
+class HomeFragment : BaseFragment(R.layout.fragment_home) {
+
+
+}
--- app/src/main/java/com/sundayting/com/spacecraft/MainActivity.kt
+++ app/src/main/java/com/sundayting/com/spacecraft/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.sundayting.com.spacecraft
 
+import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sundayting.com.ui.BaseActivity
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(R.layout.activity_main) {
 
     private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment).navController.let {
+            navController = it
+            findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(it)
+            it.addOnDestinationChangedListener { _, _, arguments ->
+                // TODO: 暂留
+            }
+        }
+    }
 
     override fun onNavigateUp() = navController.navigateUp()
 
